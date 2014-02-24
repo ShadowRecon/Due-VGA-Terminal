40700ae92cc61d9a4e7f67f26886565087881b2e
Cody A Jones <GraysCreek@ymail.com>
2014-02-24 10:44:46 -0600

    Update math.h

--- headers/math.h
+++ headers/math.h
@@ -1,1 +1,32 @@
-// Reserved for math functions
+#include <math.h>
+
+#define PI 3.14159265359
+
+const unsigned long PID180 = PI/180; 
+const unsigned long 180DPI = 180/PI;
+
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+float degrees(float Radians)
+Contributors: ShadowRecon.
+Description: Returns radians in degrees.
+Date Created: Feb 24th, 2014. By ShadowRecon.
+Last Modified: Feb 24th, 2014. By ShadowRecon.
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+float degrees(float Radians)
+{
+  return ((180DPI) * Radians);
+}
+
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+float radians(floar Degrees)
+Contributors: ShadowRecon.
+Description: Returns degrees in radians.
+Date Created: Feb 24th, 2014. By ShadowRecon.
+Last Modified: Feb 24th, 2014. By ShadowRecon.
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+float radians(floar Degrees)
+{
+  return ((PID180) * Degrees);
+}

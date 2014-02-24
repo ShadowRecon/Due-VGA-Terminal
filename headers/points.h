03fd5c744acf10f498d05b408daa68880606800b
Cody A Jones <GraysCreek@ymail.com>
2014-02-24 10:57:08 -0600

    Update points.h

--- headers/points.h
+++ headers/points.h
@@ -1,5 +1,7 @@
-#include "Arduino.h"
 #include <math.h>
+
+#include "Arduino.h"
+#include "math.h"
 
 struct TPoint 
 {
@@ -7,13 +9,13 @@ struct TPoint
   int y;
 };
 
-{=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 TPoint point(int x; int y);
 Contributors: ShadowRecon.
 Description: Creates a TPoint from x and y.
 Date Created: June 3rd, 2013. By ShadowRecon.
 Last Modified: June 3rd, 2013. By ShadowRecon.
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
 TPoint point(int x; int y) 
 {
@@ -22,13 +24,13 @@ TPoint point(int x; int y)
   _p.y = y;
 }
 
-{=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-int Distance(TPoint P1, TPointp P2);
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+int Distance(TPoint P1, TPoint p2);
 Contributors: ShadowRecon.
 Description: Returns distance between 2 TPoints.
 Date Created: June 3rd, 2013. By ShadowRecon.
 Last Modified: June 3rd, 2013. By ShadowRecon.
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
 int Distance(TPoint P1, TPoint p2)
 {
@@ -36,32 +38,42 @@ int Distance(TPoint P1, TPoint p2)
 }
 
 
-{=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-float getPointAngle(TPoint P1, TPoint P2)
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+float getPointAngleRadians(TPoint P1, TPoint P2)
 Contributors: ShadowRecon.
 Description: Returns angle in radians between 2 TPoints.
 Date Created: June 3rd, 2013. By ShadowRecon.
 Last Modified: June 3rd, 2013. By ShadowRecon.
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
 float getPointAngleRadians(TPoint P1, TPoint P2)
 {
-  return aTan2(P1.Y - P2.Y, P1.X - P2.X)
+  return aTan2(P1.Y - P2.Y, P1.X - P2.X);
 }
 
-{=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 float getPointAngleDegrees(TPoint P1, TPoint P2)
 Contributors: ShadowRecon.
 Description: Returns angle in degrees between 2 TPoints.
 Date Created: June 3rd, 2013. By ShadowRecon.
 Last Modified: June 3rd, 2013. By ShadowRecon.
-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=}
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
 
 float getPointAngleDegrees(TPoint P1, TPoint P2)
 {
-  return aTan2(P1.Y - P2.Y, P1.X - P2.X)
+  return Degrees(aTan2(P1.Y - P2.Y, P1.X - P2.X));
 }
 
+/*=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+float getPointAngleDegrees(TPoint P1, TPoint P2)
+Contributors: ShadowRecon.
+Description: Returns angle in degrees between 2 TPoints.
+Date Created: June 3rd, 2013. By ShadowRecon.
+Last Modified: June 3rd, 2013. By ShadowRecon.
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+Result.X := Round(mx + cos(angle) * (p.x - mx) - sin(angle) * (p.y - my));  
+Result.Y := Round(my + sin(angle) * (p.x - mx) + cos(angle) * (p.y- my));
 
 
 
